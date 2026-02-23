bd038fbae19a6d563110b0f4d543856023887b5f
olamelas <olamelas@nexusgeographics.com>
2026-02-23 13:05:34 +0100

    Initial commit

---
+++ src/main/java/com/sitmun/padro/PadroApplication.java
@@ -0,0 +1,13 @@
+package com.sitmun.padro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sitmun/padro/PadroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitmun.padro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
